fdb16a51043726f93528d21f22fd0444199ffa20
Dillon <dillon.pena.0@gmail.com>
2017-10-24 20:10:17 -0700

    initial commit

--- src/dpgen.cpp
+++ src/dpgen.cpp
@@ -1,27 +1,10 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
 #include <vector>
 #include <string>
 
 using namespace std;
-
-class token
-{
-	enum class {
-		ID,
-		SUM,
-		MULT,
-		DIV,
-		MOD,
-		//etc.
-	};
-};
-
-vector<token>
-ParseTokens(string filename)
-{
-	return {};
-}
-
 
 int main(int argc, char *argv[])
 {
@@ -30,9 +13,20 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 
-	vector<token> netlistTokens{ ParseTokens(argv[1]) };
-	//parsing netlistParsing{netlistFile{argv[1],ifstream::in}};
-	
-	cout << "done" << endl;
+	ifstream infile(argv[1]);
+	for (string line; getline(infile, line);) {
+		if (line[0] == '/' && line[1] == '/')
+			continue;
+
+		istringstream iss(line);
+
+		string first;
+
+		iss >> first;
+	}
+	infile.close();
+
+	ofstream outfile(argv[2]);
+
 	return 0;
 }
